1321af1206634b034dd67d3be865ed4cf7562e23
CaloMorell <oscarmorell07@yahoo.com>
2023-06-20 00:08:51 +0000

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,9 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - This program will assign a random number to the variable n 
- * each time it is executed. Complete the source code in 
- * order to print the last digit of the number stored in the variable n.
+ * main - Prints the last digit of the number stored in the variable n.
  *
  * Return: Always 0.
  */
