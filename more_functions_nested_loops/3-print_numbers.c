81d6d5eb365744dc890f8cfa4e2537ae04d6fe04
CaloMorell <oscarmorell07@yahoo.com>
2023-06-24 03:09:40 +0000

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - That prints the numbers, from 0 to 9.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (1 = 0; i < 10; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
