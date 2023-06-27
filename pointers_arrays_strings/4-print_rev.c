5303e9c726cab4173b427d26f9947490f8612b18
CaloMorell <oscarmorell07@yahoo.com>
2023-06-27 22:58:21 +0000

    I can only go one way. I've not got a reverse gear

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,15 +10,16 @@ void print_rev(char *s)
 {
 
 	int length = 0;
-    while (s[length] != '\0') 
+	while (s[length] != '\0')
+	
 	{
         length++;
     }
-
-    for (int i = length - 1; i >= 0; i--) 
+	
+	for (int i = length - 1; i >= 0; i--)
+	
 	{
-        printf("%c", s[i]);
+		printf("%c", s[i]);
     }
-
-    printf("\n");
+	printf("\n");
 }
