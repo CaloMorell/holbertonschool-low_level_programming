eb63760244593a64553593b8dd77dae91dee5e83
CaloMorell <oscarmorell07@yahoo.com>
2023-06-30 17:47:23 +0000

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,42 +2,37 @@
 #include <stdio.h>
 
 /**
- * cap_string - Capitalizes all words in a string.
- * @str: The string to capitalize.
+ * cap_string - all words in a string.
+ * @str: The string
  *
- * Return: The capitalized string.
+ * Return: A pointer to the changed string.
  */
-char *cap_string(char *str)
+
+char *cap_string(char *)
 {
 	int i = 0;
-	int capitalize_next = 1;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
+		whilw (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++
 
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-				str[i] == '}')
-		{
-			capitalize_next = 1;
-		}
-		else
-		{
-
-			if (capitalize_next && islower(str[i]))
-			{
-				str[i] = toupper(str[i]);
-				capitalize_next = 0;
-			}
-			else
-			{
-				capitalize_next = 0;
-			}
-		}
-
-		i++;
+			if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				i == 0)
+				str[i] -= 32;
+		i++
 	}
 
 	return (str);
