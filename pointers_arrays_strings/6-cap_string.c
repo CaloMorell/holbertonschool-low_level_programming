0a8f467c7cebcb756f78f77001a9e8b574133d7e
CaloMorell <oscarmorell07@yahoo.com>
2023-06-30 16:38:53 +0000

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,37 +9,36 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
-    int capitalize_next = 1;
+	int i = 0;
+	int capitalize_next = 1;
 
-    while (str[i] != '\0')
-    {
+	while (str[i] != '\0')
+	{
 
-        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-            str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-            str[i] == '}')
-        {
-            capitalize_next = 1;
-        }
-        else
-        {
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+				str[i] == '}')
+		{
+			capitalize_next = 1;
+		}
+		else
+		{
 
-            if (capitalize_next && islower(str[i]))
-            {
-                str[i] = toupper(str[i]);
-                capitalize_next = 0;
-            }
-            else
-            {
-                capitalize_next = 0;
-            }
-        }
+			if (capitalize_next && islower(str[i]))
+			{
+				str[i] = toupper(str[i]);
+				capitalize_next = 0;
+			}
+			else
+			{
+				capitalize_next = 0;
+			}
+		}
 
-        i++;
-    }
+		i++;
+	}
 
-    return (str);
+	return (str);
 }
-
