6f1328c551ba4d02b625a2e1444671a7b5b85e0e
CaloMorell <oscarmorell07@yahoo.com>
2023-07-07 11:52:59 +0000

    4. To infinity and beyond

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,45 +1,43 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - Program that adds positive numbers
-
- * @argc: This is the argument count
- * @argv: This is the argument vector
+ * main - Programa que suma números positivos
+ *
+ * @argc: Cantidad de argumentos
+ * @argv: Vector de argumentos
  *
  * Return: 0;
  */
-
-
 int main(int argc, char *argv[])
 {
-	int sum = 0;
+    int sum = 0;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
+    if (argc == 1)
+    {
+        printf("0\n");
+        return (0);
+    }
 
-	for (int i = 1; i < argc; i++)
-	{
-		char *num = argv[i];
-		int j = 0;
+    for (int i = 1; i < argc; i++)
+    {
+        char *num = argv[i];
+        int j = 0;
 
-		while (num[j] != '\0')
-		{
-			if (!isdigit(num[j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			j++;
-		}
+        while (num[j] != '\0')
+        {
+            if (!isdigit(num[j]))
+            {
+                printf("Error\n");
+                return (1);
+            }
+            j++;
+        }
 
-		sum += atoi(num);
-	}
+        sum += atoi(num);
+    }
 
-	printf("%d\n", sum);
-	return (0);
+    printf("%d\n", sum);
+    return (0);
 }
