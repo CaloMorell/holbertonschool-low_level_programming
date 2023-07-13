249b8895193500f71518dc3d4ec7d166169394a0
CaloMorell <oscarmorell07@yahoo.com>
2023-07-13 13:31:48 +0000

    3. If you even dream of beating me you'd better wake up and apologize

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - function that returns a pointer to a 2 dimensional array of integers.
+ * alloc_grid - function that returns a pointer to a 2 
+ * dimensional array of integers.
  *
  * @width: 0 or negative.
  * @height: 0 or negative.
