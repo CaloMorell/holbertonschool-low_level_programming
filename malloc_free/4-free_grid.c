f35f2e4144c825c97e18853a503657f83f4d3ce5
CaloMorell <oscarmorell07@yahoo.com>
2023-07-13 15:11:10 +0000

    4. It's not bragging if you can back it up

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by your alloc_grid function.
+ * free_grid - frees a 2 dimensional grid previously created
+ * by your alloc_grid function.
  *
- * @grid:
- * @height:
+ * @grid: dimesional array.
+ * @height: number array.
  *
  */
 
