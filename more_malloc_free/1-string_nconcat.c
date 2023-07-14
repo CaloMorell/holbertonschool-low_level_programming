1b8db785e0c949c2aeb90cdc5130519b01ab46e0
CaloMorell <oscarmorell07@yahoo.com>
2023-07-14 01:27:31 +0000

    1. string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,32 +15,32 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j;
-char *ptr;
+	unsigned int i, x;
+	char *s3;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-if (n > strlen(s2))
-n = strlen(s2);
+	if (n > strlen(s2))
+		n = strlen(s2);
 
-s3 = malloc(strlen(s1) + n + 1);
+	s3 = malloc(strlen(s1) + n + 1);
 
-if (s3 == NULL)
-return (NULL);
+	if (s3 == NULL)
+		return (NULL);
 
-for (i = 0; s1[i] != '\0'; i++)
-{
-s3[i] = s1[i];
-}
-for (x = 0; s2[x] != '\0' && x < n; x++; i++)
-{
-s3[i] = s2[x];
-}
-s3[i] = '\0';
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s3[i] = s1[i];
+	}
+	for (x = 0; s2[x] != '\0' && x < n; x++; i++)
+	{
+		s3[i] = s2[x];
+	}
+	s3[i] = '\0';
 
-return (s3);
+	return (s3);
 }
 
