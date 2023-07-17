767de5a59b1aa48953f8635ee893aa3affb1094b
CaloMorell <oscarmorell07@yahoo.com>
2023-07-17 23:30:32 +0000

    0. Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Define a new type struct dog.
+ *
+ * Return - 0.
+ */
+
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+
+};
+
+int main()
+{
+  struct dog myDog;
+
+  myDog.name = "Poppy";
+  myDog.age = 5;
+  myDog.owner = "Bob";
+
+  printf("name the dog: %s\n", myDog.name);
+  printf("age the dog: %f\n", myDog.age);
+  printf("owner the dog: %s\n", myDog.owner);
+
+  return (0);
+}
