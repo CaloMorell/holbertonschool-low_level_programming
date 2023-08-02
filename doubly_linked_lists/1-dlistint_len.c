0633e2c6d92db277c30485439a56bfb8617e8815
CaloMorell <oscarmorell07@yahoo.com>
2023-08-02 18:55:59 +0000

    1. List length

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -9,13 +9,20 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
+	/*Se inicializa una variable llamada 'count'*/
+	/*para contar el número de elementos.*/
 	size_t count = 0;
 
+	/*Mientras el nodo actual no sea NULL*/
+	/*(es decir, no se haya llegado al final de la lista)*/
 	while (h != NULL)
 	{
+		/*Avanza al siguiente nodo en la lista.*/
 		h = h->next;
+		/*Incrementa el contador en uno para contar el nodo actual.*/
 		count++;
 	}
-
+	/*Devuelve el total de elementos contados,*/
+	/*es decir, la longitud de la lista.*/
 	return (count);
 }
