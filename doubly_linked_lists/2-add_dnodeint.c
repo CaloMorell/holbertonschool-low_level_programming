f81f4cca3448ea5578218e73ed4b40125c7ab20b
CaloMorell <oscarmorell07@yahoo.com>
2023-08-02 17:29:43 +0000

    2. Add node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,32 +8,32 @@
  * Return: address of new element or NULL for failure
  */
 
-// Función para agregar un nuevo nodo al comienzo de la lista
+/* Función para agregar un nuevo nodo al comienzo de la lista*/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	// Crear un nuevo nodo
+	/*Crear un nuevo nodo*/
 	new_node = malloc(sizeof(dlistint_t *));
 	if (new_node == NULL)
 	{
-		// Si falló la asignación de memoria, retornar NULL
+		/*Si falló la asignación de memoria, retornar NULL*/
 		return (NULL);
 	}
 
-	// Configurar el valor del nuevo nodo y sus punteros
+	/*Configurar el valor del nuevo nodo y sus punteros*/
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
 
-	// Si la lista no está vacía, actualizar el nodo anterior del actual head
+	/*Si la lista no está vacía, actualizar el nodo anterior del actual head*/
 	if (*head != NULL)
 	{
 		(*head)->prev = new_node;
 	}
 
-	// Actualizar la cabeza de la lista para que apunte al nuevo nodo
+	/*Actualizar la cabeza de la lista para que apunte al nuevo nodo*/
 	*head = new_node;
 
-	// Retornar la dirección del nuevo elemento
+	/*Retornar la dirección del nuevo elemento*/
 	return (new_node);
 }
 
