5709059ee4c961c6924a54f41b83b4d0c2d37e45
CaloMorell <oscarmorell07@yahoo.com>
2023-08-02 18:22:20 +0000

    2. Add node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,34 +10,36 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node, *tmp;
+	dlistint_t *new_node, *tmp;
 
-    /*Verificar si el puntero a la cabeza de la lista es nulo*/
-    if (head == NULL)
-        return (NULL);
+	/*Verificar si el puntero a la cabeza de la lista es nulo*/
+	if (head == NULL)
+		return (NULL);
 
-    /*Asignar memoria para el nuevo nodo*/
-    new_node = malloc(sizeof(dlistint_t *));
-    if (new_node == NULL)
-        return (NULL);
+	/*Asignar memoria para el nuevo nodo*/
+	new_node = malloc(sizeof(dlistint_t *));
+	if (new_node == NULL)
+		return (NULL);
 
-    /*Configurar el valor del nuevo nodo y su puntero previo*/
-    new_node->n = n;
-    new_node->prev = NULL;
+	/*Configurar el valor del nuevo nodo y su puntero previo*/
+	new_node->n = n;
+	new_node->prev = NULL;
 
-    /*Guardar la dirección del nodo actual de la cabeza en 'tmp'*/
-    tmp = *head;
+	/*Guardar la dirección del nodo actual de la cabeza en 'tmp'*/
+	tmp = *head;
 
-    /*Hacer que el puntero 'next' del nuevo nodo apunte al nodo actual de la cabeza*/
-    new_node->next = tmp;
+	/*Hacer que el puntero 'next' del nuevo nodo apunte
+	 * al nodo actual de la cabeza*/
+	new_node->next = tmp;
 
-    /*Actualizar la cabeza de la lista para que apunte al nuevo nodo*/
-    *head = new_node;
+	/*Actualizar la cabeza de la lista para que apunte al nuevo nodo*/
+	*head = new_node;
 
-    /*Si el nodo actual de la cabeza no es nulo, actualizar su puntero previo para que apunte al nuevo nodo*/
-    if (tmp)
-        tmp->prev = *head;
+	/*Si el nodo actual de la cabeza no es nulo,
+	 * actualizar su puntero previo para que apunte al nuevo nodo*/
+	if (tmp)
+		tmp->prev = *head;
 
-    /*Retornar la dirección del nuevo nodo agregado*/
-    return (new_node);
+	/*Retornar la dirección del nuevo nodo agregado*/
+	return (new_node);
 }
