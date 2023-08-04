4e284e2e173b3f9240719915a54033a92044e2dd
CaloMorell <oscarmorell07@yahoo.com>
2023-08-04 13:18:47 +0000

    2. 10

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,12 +11,23 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int dig, lim = 1;
 
+	// Obtiene el número máximo de bits disponibles en el tipo unsigned long int.
 	dig = (sizeof(unsigned long int) * 8) - 1;
+	
+	// Verifica si el número n es igual a 0. Si es así, no importa la posición del bit, y devuelve 0.
 	if (n == 0)
 		return (0);
+
+	// Establece el bit más a la izquierda de la variable "lim" en la posición determinada por "index".
 	lim = lim << index;
+	
+	// Verifica si el índice especificado está fuera del rango de bits disponibles en el tipo unsigned long int.
+	// Si es así, devuelve -1 para indicar un error.
 	if (index > dig)
 		return (-1);
+	
+	// Comprueba si el bit en la posición "index" del número "n" es 1 o 0 y devuelve el resultado.
+	// Realiza una operación AND entre "lim" y "n" para extraer el valor del bit en la posición "index".
 	if (lim & n)
 		return (1);
 	else
