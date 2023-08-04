d07747fb0f791cab1c0c06a3f36dabdaf88bfbe9
CaloMorell <oscarmorell07@yahoo.com>
2023-08-04 13:15:58 +0000

    2. 10

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,9 +11,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int dig, lim = 1;
 
-	dig = (sizeof(unsigned long int)* 8) - 1;
+	dig = (sizeof(unsigned long int) * 8) - 1;
 	if (n == 0)
-		return(0);
+		return (0);
 	lim = lim << index;
 	if (index > dig)
 		return (-1);
