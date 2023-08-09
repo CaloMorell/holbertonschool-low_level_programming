f7a2dae1cf5d841b4ab8ecae8b134102a04702bc
CaloMorell <oscarmorell07@yahoo.com>
2023-08-09 18:25:46 +0000

    1. Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,36 +11,40 @@
  * Return: 1 on success, -1 on failure
  */
 
+// Función para crear un archivo y escribir un contenido de texto en él.
+// Recibe el nombre del archivo (filename) y el contenido de texto (text_content).
 int create_file(const char *filename, char *text_content)
 {
-	int fd, rd, wr;
+    int fd, rd, wr;  // Declaración de variables enteras para los descriptores de archivo, lectura y escritura.
 
-	if (!filename)
-		return (-1);
+    // Verifica si se proporcionó un nombre de archivo válido.
+    if (!filename)
+        return (-1);
 
+    // Abre el archivo en modo de creación (O_CREAT), escritura (O_WRONLY) y truncamiento (O_TRUNC).
+    // Los permisos se establecen en 00600 (lectura y escritura para el propietario).
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+    if (fd == -1)  // Comprueba si hubo un error al abrir el archivo.
+        return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
-	if (fd == -1)
-		return (-1);
+    // Verifica si se proporcionó contenido de texto.
+    if (text_content)
+    {
+        // Calcula la longitud del contenido de texto.
+        for (rd = 0; *(text_content + rd);)
+            rd++;
 
+        // Escribe el contenido de texto en el archivo.
+        wr = write(fd, text_content, rd);
+        if (wr != rd)  // Comprueba si la escritura fue exitosa.
+        {
+            close(fd);  // Cierra el archivo en caso de error.
+            return (-1);
+        }
+    }
 
-	if (text_content)
-	{
+    // Cierra el archivo después de escribir o en caso de que no haya contenido de texto.
+    close(fd);
 
-		for (rd = 0; *(text_content + rd);)
-			rd++;
-
-
-		wr = write(fd, text_content, rd);
-		if (wr != rd)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-
-
-	close(fd);
-
-	return (1);
+    return (1);  // Indica que el proceso se completó exitosamente.
 }
