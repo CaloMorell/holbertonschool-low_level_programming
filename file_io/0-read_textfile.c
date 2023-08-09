53eb45420567da3f136bdb3e6cd5b95a34740552
CaloMorell <oscarmorell07@yahoo.com>
2023-08-09 15:18:01 +0000

    0. Tread lightly, she is near

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,39 +12,39 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fi, re, wr;
-	char *buf;
+    int fi, re, wr;     // Variables para los descriptores de archivo y para guardar resultados
+    char *buf;          // Puntero para almacenar el contenido del archivo
 
-	if (!filename || letters == 0)
-		return (0);
+    if (!filename || letters == 0)
+        return (0);     // Si no se proporciona un nombre de archivo válido o el número de letras es 0, no se hace nada
 
-	fi = open(filename, O_RDONLY);
-	if (fi < 0)
-		return (0);
+    fi = open(filename, O_RDONLY);   // Abre el archivo en modo de solo lectura
+    if (fi < 0)
+        return (0);     // Si no se puede abrir el archivo, retorna 0
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
-		close(fi);
-		return (0);
-	}
+    buf = malloc(sizeof(char) * letters); // Asigna memoria para almacenar el contenido del archivo
+    if (buf == NULL)
+    {
+        close(fi);
+        return (0);     // Si no se puede asignar memoria, cierra el archivo y retorna 0
+    }
 
-	re = read(fi, buf, letters);
-	close(fi);
+    re = read(fi, buf, letters);    // Lee hasta 'letters' bytes del archivo y los almacena en 'buf'
+    close(fi);                      // Cierra el archivo después de la lectura
 
-	if (re < 0)
-	{
-		free(buf);
-		return (0);
-	}
+    if (re < 0)
+    {
+        free(buf);
+        return (0);     // Si la lectura fue problemática, libera la memoria y retorna 0
+    }
 
-	wr = write(STDOUT_FILENO, buf, re);
-	if (wr <= 0)
-	{
-		free(buf);
-		return (0);
-	}
+    wr = write(STDOUT_FILENO, buf, re);   // Escribe el contenido leído en la salida estándar (pantalla)
+    if (wr <= 0)
+    {
+        free(buf);
+        return (0);     // Si la escritura tuvo problemas, libera la memoria y retorna 0
+    }
 
-	free(buf);
-	return (wr);
+    free(buf);          // Libera la memoria asignada para el buffer
+    return (wr);        // Retorna el número de bytes escritos en la salida estándar (éxito)
 }
