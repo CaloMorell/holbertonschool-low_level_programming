ee95f7deb012e236982950dc8c85105275e44794
CaloMorell <oscarmorell07@yahoo.com>
2023-08-10 12:05:23 +0000

    0. >>> ht = {}

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,25 +6,33 @@
   * @size: size of the array
   * Return: the newly created hash table
   */
+
+// Función para crear una nueva tabla hash
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	hash_node_t **array;
-	unsigned long int i;
+    hash_table_t *hash_table; // Declaración de puntero a estructura hash_table_t
+    hash_node_t **array; // Declaración de un arreglo de punteros a nodos de hash
+    unsigned long int i; // Variable de iteración
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
-		return (NULL);
+    // Asignar memoria para la estructura de la tabla hash
+    hash_table = malloc(sizeof(hash_table_t));
+    if (hash_table == NULL)
+        return (NULL); // Retornar NULL en caso de fallo en la asignación de memoria
 
-	array = malloc(sizeof(hash_node_t *) * size);
-	if (array == NULL)
-		return (NULL);
+    // Asignar memoria para el arreglo de punteros a nodos de hash
+    array = malloc(sizeof(hash_node_t *) * size);
+    if (array == NULL) {
+        free(hash_table); // Liberar la memoria previamente asignada
+        return (NULL); // Retornar NULL en caso de fallo en la asignación de memoria
+    }
 
-	for (i = 0; i < size; i++)
-		array[i] = NULL;
+    // Inicializar cada elemento del arreglo como NULL (tabla vacía)
+    for (i = 0; i < size; i++)
+        array[i] = NULL;
 
-	hash_table->size = size;
-	hash_table->array = array;
+    // Configurar el tamaño de la tabla hash y el arreglo en la estructura hash_table_t
+    hash_table->size = size;
+    hash_table->array = array;
 
-	return (hash_table);
+    return (hash_table); // Retornar el puntero a la nueva tabla hash creada
 }
