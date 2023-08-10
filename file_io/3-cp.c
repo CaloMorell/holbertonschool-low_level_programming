1fa5b6ddde3f5e34a96c417e4a6d6ff1d8762618
CaloMorell <oscarmorell07@yahoo.com>
2023-08-10 11:07:40 +0000

    3. cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,23 +14,29 @@ void error_close(int, char *);
  * @file_from: file to copy from
  * Return: 0
  */
+
+// Función para copiar un archivo a otro
 int copy_file(char *file_to, char *file_from)
 {
 	int to, from, wr, err, re;
 	char *buf;
 
+	// Se asigna memoria para un buffer de lectura/escritura
 	buf = malloc(sizeof(char) * 1024);
 	if (!buf)
 		return (0);
 
+	// Se abre el archivo de origen en modo lectura
 	from = open(file_from, O_RDONLY);
 	if (from == -1)
 		error_file_from(file_from, buf);
 
+	// Se abre o crea el archivo de destino en modo escritura
 	to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to == -1)
 		error_file_to(file_to, buf);
 
+	// Se lee una cantidad de datos del archivo de origen y se copian al archivo de destino
 	re = read(from, buf, 1024);
 
 	do {
@@ -38,12 +44,15 @@ int copy_file(char *file_to, char *file_from)
 			error_file_from(file_from, buf);
 		if (re == 0)
 			break;
+		// Se escribe la cantidad de datos leídos en el archivo de destino
 		wr = write(to, buf, re);
 		if (wr == -1)
 			error_file_to(file_to, buf);
+		// Se vuelve a leer del archivo de origen
 		re = read(from, buf, 1024);
 	} while (re > 0);
 
+	// Se cierran los archivos
 	err = close(to);
 	if (err == -1)
 		error_close(to, buf);
@@ -51,6 +60,7 @@ int copy_file(char *file_to, char *file_from)
 	if (err == -1)
 		error_close(from, buf);
 
+	// Se libera la memoria asignada al buffer
 	free(buf);
 	return (0);
 }
@@ -61,10 +71,14 @@ int copy_file(char *file_to, char *file_from)
  * @buf: buffer to free
  * Return: void
  */
+// Función para manejar el error de cierre de archivo
 void error_close(int fd, char *buf)
 {
 	free(buf);
+	// Liberar la memoria asignada para el buffer
+	// Imprimir mensaje de error en la salida de error estándar (stderr)
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	// Salir del programa con un código de error específico (100)
 	exit(100);
 }
 
@@ -74,10 +88,14 @@ void error_close(int fd, char *buf)
  * @buf: buffer to free
  * Return: void
  */
+// Función para manejar el error de lectura desde archivo de origen
 void error_file_from(char *file_from, char *buf)
 {
+	// Liberar la memoria asignada para el buffer
 	free(buf);
+	// Imprimir mensaje de error en la salida de error estándar (stderr)
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	// Salir del programa con un código de error específico (98)
 	exit(98);
 }
 
@@ -87,10 +105,14 @@ void error_file_from(char *file_from, char *buf)
  * @buf: buffer to free
  * Return: void
  */
+// Función para manejar el error de escritura a archivo de destino
 void error_file_to(char *file_to, char *buf)
 {
+	// Liberar la memoria asignada para el buffer
 	free(buf);
+	// Imprimir mensaje de error en la salida de error estándar (stderr)
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	// Salir del programa con un código de error específico (99)
 	exit(99);
 }
 
@@ -100,13 +122,16 @@ void error_file_to(char *file_to, char *buf)
  * @argv: array of arguments
  * Return: 0
  */
+// Función principal (main) del programa
 int main(int argc, char *argv[])
 {
+	// Verifica si se proporcionan los argumentos correctos
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	// Llama a la función para copiar el archivo
 	copy_file(argv[2], argv[1]);
 	return (0);
 }
