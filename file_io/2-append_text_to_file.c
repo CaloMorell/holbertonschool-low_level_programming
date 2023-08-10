7a97f86c620870e7deadb148837dc6ce4b2fc2fb
CaloMorell <oscarmorell07@yahoo.com>
2023-08-10 10:37:43 +0000

    2. Speak gently, she can hear

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -6,26 +6,40 @@
  * @text_content: content to append
  * Return: 1 on success, -1 on failure
  */
+
+// Función para agregar contenido de texto a un archivo.
+// Recibe el nombre del archivo y el contenido de texto a agregar.
+// Retorna 1 en caso de éxito y -1 en caso de error.
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, wr, len;
+    int file, wr, len;
 
-	if (!filename)
-		return (-1);
-	file = open(filename, O_WRONLY | O_APPEND);
-	if (file == -1)
-		return (-1);
-	if (text_content)
-	{
-		for (len = 0; *(text_content + len);)
-			len++;
-		wr = write(file, text_content, len);
-		if (wr == -1)
-		{
-			close(file);
-			return (-1);
-		}
-	}
-	close(file);
-	return (1);
-}
+    // Verifica si el nombre del archivo es nulo.
+    if (!filename)
+        return (-1);
+
+    // Abre el archivo en modo de escritura y agregado (append).
+    file = open(filename, O_WRONLY | O_APPEND);
+    if (file == -1)
+        return (-1);
+
+    // Si hay contenido de texto para agregar.
+    if (text_content)
+    {
+        // Calcula la longitud del contenido de texto.
+        for (len = 0; *(text_content + len);)
+            len++;
+
+        // Escribe el contenido de texto en el archivo.
+        wr = write(file, text_content, len);
+        if (wr == -1)
+        {
+            // Si ocurre un error al escribir, cierra el archivo y retorna un código de error.
+            close(file);
+            return (-1);
+        }
+    }
+
+    // Cierra el archivo y retorna un código de éxito.
+    close(file);
+    return (1);
