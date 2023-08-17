141a796d29ffcb77a5fc9b9b29f913c2f053c610
CaloMorell <oscarmorell07@yahoo.com>
2023-08-17 12:16:17 +0000

    1. djb2

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,33 +1,20 @@
 #include "hash_tables.h"
 
 /**
-  * hash_djb2 - creates a hash with djb2 algorithm
-  *
-  * @str: the string key to create a hash for
-  * Return: the generated hash
-  */
-
-// La función hash_djb2 toma un puntero a una cadena de caracteres sin firmar (unsigned char)
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    // Inicializa el valor de hash con 5381, un número elegido arbitrariamente
-    unsigned long int hash = 5381;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    // Itera a través de la cadena hasta llegar al final ('\0')
-    while (*str)
-    {
-        // Obtiene el valor ASCII del carácter actual y lo almacena en 'c'
-        c = *str++;
-
-        // La operación de hash: hash * 33 + c
-        // Se desplaza el valor hash 5 bits a la izquierda y se le suma el valor original del hash.
-        // Luego se suma el valor ASCII del carácter actual 'c'.
-        hash = ((hash << 5) + hash) + c;
-
-        // En resumen, esta operación realiza una especie de multiplicación por 33 y suma el valor ASCII del carácter actual.
-    }
-
-    // Retorna el valor hash resultante después de procesar toda la cadena
-    return hash;
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
